7d0bb6b69beb24e6399e03ef48a0f95b38a835ab
stan-2021131 <sergiocoromac@gmail.com>
2026-05-15 01:25:57 -0600

    Se agregó eslint al proyecto junto a su configuración inicial

--- Frontend/eslint.config.js
+++ Frontend/eslint.config.js
@@ -1,22 +1,56 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+
+export default [
+
+    {
+        ignores: ["dist"],
     },
-  },
-])
+
+    {
+        files: ["**/*.{ts,tsx}"],
+
+        languageOptions: {
+            parser: tsParser,
+
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+
+                ecmaVersion: "latest",
+                sourceType: "module",
+            },
+
+            globals: globals.browser,
+        },
+
+        plugins: {
+            "@typescript-eslint": tsPlugin,
+            "react-hooks": reactHooks,
+            "react-refresh": reactRefresh,
+        },
+
+        rules: {
+            ...js.configs.recommended.rules,
+
+            ...tsPlugin.configs.recommended.rules,
+
+            ...reactHooks.configs.recommended.rules,
+
+            "react-refresh/only-export-components": [
+                "warn",
+                { allowConstantExport: true },
+            ],
+
+            "@typescript-eslint/no-explicit-any": "off",
+            "react-hooks/set-state-in-effect": "off",
+        },
+    },
+];
